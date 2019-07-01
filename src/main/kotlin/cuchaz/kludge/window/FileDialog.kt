402e6634e25ab83019f92aa4c76c6af8f470741f
Jeff Martin <jeff@cuchazinteractive.com>
2019-07-01 15:11:43 -0400

    added friendly wrappers for native file dialog

---
+++ src/main/kotlin/cuchaz/kludge/window/FileDialog.kt
@@ -0,0 +1,89 @@
+package cuchaz.kludge.window
+
+import cuchaz.kludge.tools.memstack
+import org.lwjgl.util.nfd.NFDPathSet
+import org.lwjgl.util.nfd.NativeFileDialog.*
+import java.nio.file.Path
+import java.nio.file.Paths
+
+
+class FilterList(vararg val filters: List<String>) {
+
+	internal val str: String? by lazy {
+		if (filters.isEmpty()) {
+			null
+		} else {
+			filters.joinToString(";") { it.joinToString(",") }
+		}
+	}
+}
+
+object FileDialog {
+
+	fun openFile(filterList: FilterList? = null, defaultPath: String? = null): Path? {
+		memstack { mem ->
+			val outPath = mem.mallocPointer(1)
+			val result = NFD_OpenDialog(filterList?.str, defaultPath, outPath)
+			when (result) {
+				NFD_OKAY -> {
+					val str = outPath.getStringUTF8(0)
+					nNFD_Free(outPath.get(0))
+					return Paths.get(str)
+				}
+				NFD_CANCEL -> return null
+				else -> throw FileDialogException("can't open dialog: open file")
+			}
+		}
+	}
+
+	fun openFiles(filterList: FilterList? = null, defaultPath: String? = null): List<Path>? {
+		memstack { mem ->
+			val pathSet = NFDPathSet.mallocStack(mem)
+			val result = NFD_OpenDialogMultiple(filterList?.str, defaultPath, pathSet)
+			when (result) {
+				NFD_OKAY -> {
+					val count = NFD_PathSet_GetCount(pathSet)
+					val paths = (0 until count).map { Paths.get(NFD_PathSet_GetPath(pathSet, it)!!) }
+					NFD_PathSet_Free(pathSet)
+					return paths
+				}
+				NFD_CANCEL -> return null
+				else -> throw FileDialogException("can't open dialog: open files")
+			}
+		}
+	}
+
+	fun saveFile(filterList: FilterList? = null, defaultPath: String? = null): Path? {
+		memstack { mem ->
+			val outPath = mem.mallocPointer(1)
+			val result = NFD_SaveDialog(filterList?.str, defaultPath, outPath)
+			when (result) {
+				NFD_OKAY -> {
+					val str = outPath.getStringUTF8(0)
+					nNFD_Free(outPath.get(0))
+					return Paths.get(str)
+				}
+				NFD_CANCEL -> return null
+				else -> throw FileDialogException("can't open dialog: save file")
+			}
+		}
+	}
+
+	fun pickFolder(defaultPath: String? = null): Path? {
+		memstack { mem ->
+			val outPath = mem.mallocPointer(1)
+			val result = NFD_PickFolder(defaultPath, outPath)
+			when (result) {
+				NFD_OKAY -> {
+					val str = outPath.getStringUTF8(0)
+					nNFD_Free(outPath.get(0))
+					return Paths.get(str)
+				}
+				NFD_CANCEL -> return null
+				else -> throw FileDialogException("can't open dialog: pick folder")
+			}
+		}
+	}
+}
+
+class FileDialogException(msg: String) : RuntimeException("$msg: ${NFD_GetError()}")
